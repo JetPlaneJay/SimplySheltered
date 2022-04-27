c72d0b8dc1a3d2c0a3faafe5d8378cf2d8528577
Jacob A. Tabares <73261191+JetPlaneJay@users.noreply.github.com>
2022-04-26 19:07:18 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,40 @@
-function openList1() {
-    var list = document.getElementById("main-nav__items");
+// function openList1() {
+//     var list = document.getElementById("main-nav__items");
 
-    if (list.style.display == "none"){
-        list.style.display = "flex";
-    }else{
-        list.style.display = "none";
-    }
-}
-
-document.querySelector(".posterImage");
+//     if (list.style.display == "none"){
+//         list.style.display = "flex";
+//     }else{
+//         list.style.display = "none";
+//     }
+// }
 
 
-window.addEventListener('scroll', function(){
-    var value = window.scrollY;
 
-    bg.style.top = -value * 0.15 + 'px'
-})
+// function openList1() {
+//     var list = document.getElementById("main-nav__items");
+
+//     if (list.style.display == "none"){
+//         list.style.display = "flex";
+//     }else{
+//         list.style.display = "none";
+//     }
+// }
+
+
+// document.querySelector(".posterImage");
+
+
+// window.addEventListener('scroll', function(){
+//     var value = window.scrollY;
+
+//     bg.style.top = -value * 0.15 + 'px'
+// })
+
 
 function myFunction() {
     var x = document.getElementById("main-nav__items");
     if (x.style.visibility === 'hidden') {
-        x.style.visibility = 'visibility';
+        x.style.visibility = 'visible';
     } else {
         x.style.visibility = 'hidden';
     }
