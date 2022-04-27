3995c5cb679865426bf2081e0856d92f92382bef
Jacob A. Tabares <73261191+JetPlaneJay@users.noreply.github.com>
2022-04-26 18:45:23 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
-// function openList1() {
-//     var list = document.getElementById("main-nav__items");
+function openList1() {
+    var list = document.getElementById("main-nav__items");
 
-//     if (list.style.display == "none"){
-//         list.style.display = "flex";
-//     }else{
-//         list.style.display = "none";
-//     }
-// }
+    if (list.style.display == "none"){
+        list.style.display = "flex";
+    }else{
+        list.style.display = "none";
+    }
+}
 
 document.querySelector(".posterImage");
 
